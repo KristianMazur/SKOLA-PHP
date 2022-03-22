6142534f54cd5d89a29a1193a1f6342e09e8bdf1
KristianMazur <75983320+KristianMazur@users.noreply.github.com>
2022-03-22 09:48:23 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Domov</title>
+
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark countainer-fluid">
+
+            <a class="navbar-brand" href="/Project//index.php">Moj web</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+
+                    </li>
+                </ul>
+
+
+
+
+
+                <div>
+                    <a href="./login.php" class="btn btn-success">Login</a>
+                    <a href="./index1.php" class="btn btn-danger">Register</a>
+                </div>
+            </div>
+
+        </nav>
+
+
+
+    </header>
